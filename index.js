802e5372d0f7add5addb162ab79a4f12ef15fc28
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-08 11:58:53 -0500

    so... Yeah, I went through all this closure's shit and the performance of my scrape is pretty much the same XD F

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 import getQuotes from "./main.js"
+import readline from 'readline'
+
+// readLine interface
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout,
+    historySize: 0,
+    prompt: ''
+}) 
+
 let n = 0, avg = []
 do {
     const start = performance.now()
 
-    // const keywords = await new Promise( 
+    // const keywords = await new Promise(
     //     resolve => rl.question(
-    //         'I do listen, human: ', resolve))
-    const okcomputer = await getQuotes('love flowers', 10)
+    //         'I do listen, human: ',
+    //         resolve
+    //     )
+    // )
+    // rl.close()
+    // console.log('...' + keywords + '?')
+    const okcomputer = await getQuotes('loneliness', 10)
     console.log(okcomputer)
     const end = performance.now()
     const elapsedTime = end - start
     console.log(`Elapsed time: ${elapsedTime}ms`)
     avg.push(elapsedTime); n++
-} while (n < 99)
+} while (n < 1)
 
 avg = avg.reduce((acc, val) => acc + val, 0) / avg.length
 console.log('\nas it is, your code roughly takes', avg, 'ms')
