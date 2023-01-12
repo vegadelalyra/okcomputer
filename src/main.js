c3f4028e7d7ceddddd1ad15ef3a288ccb5e518b3
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-12 11:09:58 -0500

    Testing out improvements

--- src/main.js
+++ src/main.js
@@ -14,25 +14,17 @@ export default function getQuotes(keyword, wordsPerPhrase = 9) {
     
     // dynamic URL and vital resources for code to succeed
     const baseURL = "https://www.brainyquote.com" 
-    let epistle = [], fetchSucceed = false
-
-    const URL = multiWords 
+    let fetchSucceed = false, URL = multiWords 
     ? `${baseURL}/search_results?q=${multiWords}` 
     : `${baseURL}/topics/${keyword}-quotes`
     
-    // Let's start the crazziness
     return closure()
-
+    
     async function closure(url) {
         // fetch and parse the target website
         const res = await fetch(url || URL)
         const html = await res.text()
         const $ = cheerio.load(html)
-        // console.log(url || URL)
-        
-        // CSS-selector  of the desired HTML element
-        const quotes = $(".oncl_q:nth-child(1) div")
-        // if (!multiWords) console.log(guardClause)
         
         if (fetchSucceed) return innerClosure()
         
@@ -40,33 +32,37 @@ export default function getQuotes(keyword, wordsPerPhrase = 9) {
         const guardClause = $('.bq-subnav-h1').text() 
         const badNews =`\nBad news! We haven't written quotes for ${keyword}\n`
         if (guardClause === '\nPage Not Found\n') return badNews + guardClause 
-
-        // second guard clause: does thou word have pages? 
+        
+        // Second guard clause: does thou word have pages? 
         const random = arr => arr[Math.floor(Math.random() * arr.length)]
         let havePages = $('.pagination-sm').text()
-        if (!!havePages) havePages = havePages.split('\n').filter(n => !isNaN(n) && !!n)
-        havePages = random(havePages)
-        console.log(havePages);
-        fetchSucceed = true; return innerClosure()        
+        if (!havePages) return innerClosure()
+
+        // pagination (code will scrape a random page)
+        havePages = havePages.split('\n').filter(n => !isNaN(n) && !!n)
+        havePages = Number(random(havePages)) + 1
+        let button = nth => $(`.page-item:nth-child(${nth}) .page-link`)
+        let clickButton = a => button(a).attr('href')
+        let page = baseURL + clickButton(havePages)
+
+        fetchSucceed = true; return closure(page)
         
-        async function innerClosure () {
-            // get the elements that matches your conditions
-            quotes.each(function() {   
-                if ($(this)
-                .text()
-                .split(' ')
-                .length < wordsPerPhrase
-                ) epistle.push($(this).text())
-            }) // console.log(quote, quote.split(' ').length, 'words')
+        function innerClosure() {
+            // CSS-selector  of the desired HTML element
+            const quotes = $(".oncl_q:nth-child(1) div")
             
-            // pagination (code will scrape until the last page)
-            let button = nth => $(`.page-item:nth-child(${nth}) .page-link`)
-            let curated = a => button(a).attr('href')
-            let nextPage = button(8).text() === 'Next' ? curated(8): curated(9)
+            // get the elements that matches your conditions
+            const epistle = [] 
+            quotes.each(function() {   
+            if ($(this)
+            .text()
+            .split(' ')
+            .length < wordsPerPhrase
+            ) epistle.push($(this).text())
+            }) 
             
             // output
-            // console.log('\n', epistle.length, 'phrases scraped from web!\n...and the chosen one is:')
-            const chosenOne = random(arr)
+            const chosenOne = random(epistle)
             return '\x1b[33m' + chosenOne + '\x1b[37m'
         }
     }
